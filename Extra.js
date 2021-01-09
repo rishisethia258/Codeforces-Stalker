b1c0bc4cc77b76ef8f6d225d47a92f147e2ee8e2
Rishi Sethia <59594395+rishisethia258@users.noreply.github.com>
2021-01-09 22:52:04 +0530

    Update Extra.js

--- Extra.js
+++ Extra.js
@@ -1,36 +1,50 @@
 const form = document.querySelector('#searchForm2');
+const btn = document.querySelector('#find');
+var users = [];
 
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const handle = form.elements.query.value;
-
-
-    const userStatus1 = await axios.get(`https://codeforces.com/api/user.status?handle=${handle}`);
-
-    var count=0;
-    var Ids=[]
-    const contests = await axios.get(`https://codeforces.com/api/contest.list?gym=false`);
-    var len=contests.data.result.length;
-    for(var j=0;j<len;j++)
-    {
-       var k=0;
-       for(var i=0;i<userStatus1.data.result.length;i++)
-      {
-          if(userStatus1.data.result[i].contestId == contests.data.result[j].id) {
-             k=1;
-             break;
-             //Ids[count]=userStatus;
-             //if(count==10) break;
-          }
-      }
-        if(k==0){
-        Ids[count]=contests.data.result[j].id;
-        count++;
-        }
-        if(count==7) break;
+    try {
+        const handle = form.elements.query.value;
+        await axios.get(`https://codeforces.com/api/user.status?handle=${handle}`);
+        console.log(handle);
+        var ul = document.querySelector("#listOfUsers");
+        ul.innerHTML += (`<li>${handle}</li>`);
+        users.push(handle);
+    } catch {
+        alert("User Not Found");
     }
+});
 
-    // console.log(Ids);
-    // console.log(contests.data.result.length);
+btn.addEventListener('click', async function (e) {
+    e.preventDefault();
+    try {
+        var userStatus = [];
+        for (var i = 0; i < users.length; i++) {
+            var v = await axios.get(`https://codeforces.com/api/user.status?handle=${users[i]}`);
+            userStatus.push(v.data.result);
+        }
+        var contests = await axios.get(`https://codeforces.com/api/contest.list?gym=false`);
+        var ids = {};
 
+        for (var i = 0; i < contests.data.result.length; i++) {
+            var u = contests.data.result[i].id;
+            ids[u] = true;
+        }
+        console.log(ids);
+        for (var i = 0; i < userStatus.length; i++) {
+            console.log(userStatus[i]);
+            for (var j = 0; j < userStatus[i].length; j++) {
+                ids[userStatus[i][j].contestId] = false;
+            }
+        }
+        var ul = document.querySelector("#ans");
+        for (i in ids) {
+            if (ids[i]) {
+                ul.innerHTML += (`<li><a href="https://codeforces.com/contests/${i}">ContestId - ${i}</a></li>`);
+            }
+        }
+    } catch {
+        alert("Some error !!!");
+    }
 });
